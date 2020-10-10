6910ea190211fee06c6ba26bd2b71fc26690f69e
zeeniye <bluemul07@gmail.com>
2020-10-10 12:03:34 +0800

    change title module2

--- MODUL2 DIFAGAMA/book_hotel.php
+++ MODUL2 DIFAGAMA/book_hotel.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Book your room</title>
+    <title>Booking</title>
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -35,7 +35,7 @@
 
     <!-- Content -->
     <div class="container-fluid">
-        <h2>oke, gooddfddfdf</h2>
+        
     </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
