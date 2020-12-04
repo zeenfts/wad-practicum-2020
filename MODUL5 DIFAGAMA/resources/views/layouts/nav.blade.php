1f969ac0a681e7e67968e5ad006f9d34091e14b3
zeeniye <bluemul07@gmail.com>
2020-12-04 11:08:55 +0800

    base view blade

---
+++ MODUL5 DIFAGAMA/resources/views/layouts/nav.blade.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-info shadow-sm">
+    <div class="container">
+        {{-- <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'Laravel') }}
+        </a> --}}
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a href="{{ route('home_prod') }}" class="nav-link">HOME</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('home_prod') }}" class="nav-link">PRODUCT</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('home_prod') }}" class="nav-link">ORDER</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('home_prod') }}" class="nav-link">HISTORY</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
